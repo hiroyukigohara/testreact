c60439441983d4996ead44a178cd88397e3750af
hiroyukigohara-plai <99313440+hiroyukigohara-plai@users.noreply.github.com>
2024-04-28 14:30:16 +0900

    prepared components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+const MyTextBox = () => {
+  return (
+    <div>
+    <input type="text" />
+    </div>
+
+  );
+}
+
+const ShowUserInput = (props) => {
+  return (
+    <div>
+      <p>Input is : {props.userInput}</p>
+    </div>
+  );
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MyTextBox />
+      <ShowUserInput userInput="Hello" />
     </div>
   );
 }
