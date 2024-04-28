1c7e26f55e6b35c416fd34c64ce2964b654f1a25
hiroyukigohara-plai <99313440+hiroyukigohara-plai@users.noreply.github.com>
2024-04-28 14:35:19 +0900

    Prepared all parts

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 const MyTextBox = () => {
+  const [value, setValue] = useState('');
   return (
     <div>
-    <input type="text" />
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
     </div>
 
   );
@@ -19,10 +25,11 @@ const ShowUserInput = (props) => {
 }
 
 function App() {
+  const [userInput, setUserInput] = useState('Hello');
   return (
     <div>
       <MyTextBox />
-      <ShowUserInput userInput="Hello" />
+      <ShowUserInput userInput={userInput} />
     </div>
   );
 }
